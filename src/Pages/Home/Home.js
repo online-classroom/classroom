e19c62c0ac36e04d495a9023c5b8666341198e06
vivian <vivian@vivians-MacBook-Pro.local>
2019-03-18 17:07:58 -0600

    added course section to home and footer

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,27 +3,37 @@ import './Home.scss'
 
 const Home =()=>{
     return(
-        <div id='Home'>
-            <div className='login-container'>
-                <div className='login-content'>
-                    <span className='login-title'>LOGIN</span>
-                    <br/>
-                    <br/>
-                    <span>Username:</span>
-                    <br/>
-                    <input className='login-input' placeholder='Username'/>
-                    <br/>
-                    <br/>
-                    <span>Password:</span>
-                    <br/>
-                    <input className='login-input' placeholder='Password'/>
+        <div className='home-container'>
+            <div id='Home'>
+                <div className='login-container'>
+                    <div className='login-content'>
+                        <span className='login-title'>LOGIN</span>
+                        <br/>
+                        <br/>
+                        <span>Username:</span>
+                        <br/>
+                        <input className='login-input' placeholder='Username'/>
+                        <br/>
+                        <br/>
+                        <span>Password:</span>
+                        <br/>
+                        <input className='login-input' placeholder='Password'/>
+                        <br/>
+                        <button className='login-button'>LOGIN</button>
+                    </div>
+                </div>
+                <div className='register-buttons'>
+                    <div className='register-button-container'>
+                        <button className='reg-button'>REGISTER AS STUDENT</button>
+                        <button className='reg-button'>REGISTER AS TEACHER</button>
+                    </div>
                 </div>
             </div>
-            <div className='register-buttons'>
-                <div className='register-button-container'>
-                    <button className='reg-button'>REGISTER AS STUDENT</button>
-                    <button className='reg-button'>REGISTER AS TEACHER</button>
-                </div>
+            <div className='courses-container'>
+                <div className='subjects'>Subject 1</div>
+                <div className='subjects'>Subject 2</div>
+                <div className='subjects'>Subject 3</div>
+                <div className='subjects'>Subject 4</div>
             </div>
         </div>
     )
