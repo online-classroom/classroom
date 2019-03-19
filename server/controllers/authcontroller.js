c78f1d9677c704292bcda5db5670a6101f771cb6
Your Name <you@example.com>
2019-03-18 18:09:00 -0600

    set up auth controller

---
+++ server/controllers/authcontroller.js
@@ -0,0 +1,81 @@
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+  register: async (req, res) => {
+    const {
+      username,
+      email,
+      password,
+      first_name,
+      last_name,
+      is_teacher
+    } = req.body;
+    const { session } = req;
+    const db = req.app.get("db");
+
+    const usernameCheck = await db.auth.getUsernames(username);
+    const emailCheck = await db.auth.getEmails(email);
+
+    if (usernameCheck.length !== 0) {
+      return res.status(409).send("Username Already Taken");
+    }
+
+    if (emailCheck.length !== 0) {
+      return res.status(409).send("Account exists with Email");
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    let newUser = await db.auth.register([
+      username,
+      email,
+      hash,
+      first_name,
+      last_name,
+      is_teacher
+    ]);
+    newUser = newUser[0];
+
+    delete newUser.password;
+    session.user = { ...newUser };
+
+    res.status(201).send(session.user);
+  },
+
+  login: async (req, res) => {
+    const { username, password } = req.body;
+    const db = req.app.get("db");
+    const { session } = req;
+
+    let user = await db.auth.login(username);
+    user = user[0];
+
+    if (!user) {
+      return res.status(404).send("Username/Email does not exist");
+    }
+
+    const foundUser = bcrypt.compareSync(password, user.password);
+
+    if (foundUser) {
+      session.user = user;
+      return res.status(200).send(session.user);
+    } else {
+      res.status(401).send("Incorrect Password! Try Again");
+    }
+  },
+
+  logout: (req, res) => {
+    req.session.destroy();
+    res.sendStatus(200);
+  },
+
+  getUser: (req, res) => {
+    const { user } = req.session;
+    if (user) {
+      res.status(200).send(user);
+    } else {
+      res.sendStatus(401);
+    }
+  }  
+};
