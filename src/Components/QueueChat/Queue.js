4097d79f789124d8f3d40f4e44f75f02a81243b8
elliot <elliotgcraig@icloud.com>
2019-03-19 11:08:31 -0600

    ready to redux it

--- src/Components/QueueChat/Queue.js
+++ src/Components/QueueChat/Queue.js
@@ -1,16 +1,59 @@
 import React from "react";
+import './Queue.scss'
 
-const Queue = () => {
-  return (
-    <div className='queue_component'>
-      <div>        
-        the Queue
+class Queue extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      peopleInQueue: [
+        {name: 'Sam', time: '4:32', question: 'What is google'},
+        {name: 'Bill', time: '4:32', question: 'How do you set up a fullstack application'},
+        {name: 'Max', time: '4:33', question: 'Please teach me PHP'},
+        {name: 'Kevin', time: '4:35'}
+      ],
+      questionText: ''
+      // yourName: 'Tom',
+      // currentTime: '10:46'
+    }
+  }
+  displayPeopelInQueue = ()=>{
+    let displayed = this.state.peopleInQueue.map((ele)=>{
+      return (
+        <div className='name_and_time'>
+          <div>
+            <span>{ele.name}</span><span>{ele.time}</span>
+          </div>
+          <div>{ele.question}</div>
+        </div>
+      )
+    })
+    return displayed
+  }
+  handleRaiseHand = ()=>{
+    let newQueueItem = {name: 'Tom', time: '10:48', question: this.state.questionText}
+    this.setState({
+      peopleInQueue: [...this.state.peopleInQueue, newQueueItem],
+      questionText: ''
+    })
+  }
+  handleTextInput = (val)=>{
+    this.setState({
+      questionText: val
+    })
+  }
+  render(){
+    return (
+      <div className='queue_component'>
+        <div>        
+          {this.displayPeopelInQueue()}
+        </div>
+        <input value={this.state.questionText} onChange={(e)=>this.handleTextInput(e.target.value)}/>
+        <button onClick={this.handleRaiseHand}>
+          Raise hand
+        </button>
       </div>
-      <button>
-        Raise hand
-      </button>
-    </div>
-  );
-};
+    )
+  }
+}
 
 export default Queue;
