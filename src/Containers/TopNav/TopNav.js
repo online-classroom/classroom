b7e5c5bd28586c8108eb7e703200a39ef8fa7fb6
vivian <vivian@vivians-MacBook-Pro.local>
2019-03-20 09:36:08 -0600

    fixed weird spacing on topnav

--- src/Containers/TopNav/TopNav.js
+++ src/Containers/TopNav/TopNav.js
@@ -7,8 +7,10 @@ const TopNav =()=>{
     return(
         <div className='topnav'>
             <span className='logo'><img src={logo} alt='Logo'/></span>
-            <span className='auth-button'>Login</span>
-            <span className='reg-button'>Register</span>
+            <div className='links-container'>
+                <span className='auth-button'>Login</span>
+                <span className='reg-button'>Register</span>
+            </div>
         </div>
     )
 }
