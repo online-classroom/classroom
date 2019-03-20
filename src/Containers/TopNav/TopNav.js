f3a1a9a9f2a3c873e3cd10b80b093db76c54c0f4
Your Name <you@example.com>
2019-03-20 07:26:52 -0600

    added logo to topnav

--- src/Containers/TopNav/TopNav.js
+++ src/Containers/TopNav/TopNav.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import logo from './../../assets/logo.png'
 import './TopNav.scss'
 
 const TopNav =()=>{
     
     return(
         <div className='topnav'>
-            <span className='logo'>Logo</span>
+            <span className='logo'><img src={logo} alt='Logo'/></span>
             <span className='auth-button'>Login</span>
             <span className='reg-button'>Register</span>
         </div>
