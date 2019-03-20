1a0680f2e02442e37f4325c176167bae93d04007
vivian <vivian@vivians-MacBook-Pro.local>
2019-03-20 10:50:47 -0600

    nav bar is looking fantastic and stuff

--- src/Containers/TopNav/TopNav.js
+++ src/Containers/TopNav/TopNav.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import logo from './../../assets/logo.png'
 import './TopNav.scss'
+import { NavLink } from 'react-router-dom'
 
 const TopNav =()=>{
     
     return(
         <div className='topnav'>
+            <div className='searchbar-container'><input className='searchbar' placeholder='SEARCH'/></div>
             <span className='logo'><img src={logo} alt='Logo'/>VLASSROOM</span>
             <div className='links-container'>
-                <span className='auth-button'>LOGIN</span>
-                <span className='reg-button'>REGISTER</span>
+                <NavLink to='/login'><span className='nav-button'>LOGIN</span></NavLink>
+                <NavLink to='/login'><span className='nav-button'>REGISTER</span></NavLink>
             </div>
         </div>
     )
