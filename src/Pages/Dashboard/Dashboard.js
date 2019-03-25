e951b3f5721c8cd67c6cb29550a00a236753e83e
Your Name <you@example.com>
2019-03-25 12:03:01 -0600

    set privacy of dashboard and classroom pages

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,22 +1,24 @@
 // started by Joe - march 18 2019
-import React, { Component } from 'react';
-import './Dashboard.scss';
-import ProfileInfoContainer from '../../Containers/ProfileInfoContainer/ProfileInfoContainer';
-import Schedule from '../../Containers/Schedule/Schedule';
-import ClassListContainer from '../../Containers/ClassListContainer/ClassListContainer';
+import React, { useEffect } from "react";
+import "./Dashboard.scss";
+import ProfileInfoContainer from "../../Containers/ProfileInfoContainer/ProfileInfoContainer";
+import Schedule from "../../Containers/Schedule/Schedule";
+import ClassListContainer from "../../Containers/ClassListContainer/ClassListContainer";
 
-export default class Dashboard extends Component {
-  render() {
-    return (
-      <div className='dashboardMainPage'>
-        <div className='dashContainer'>
-          <ProfileInfoContainer />
-          <ClassListContainer />
-        </div>
-        <div className='courseListContainer'>
-          <Schedule />
-        </div>
+const Dashboard = () => {
+
+  return (
+    <div className="dashboardMainPage">
+      <div className="dashContainer">
+        <ProfileInfoContainer />
+        <ClassListContainer />
       </div>
-    );
-  }
-}
+      <div className="courseListContainer">
+        <Schedule />
+      </div>
+    </div>
+  );
+};
+
+
+export default Dashboard;
