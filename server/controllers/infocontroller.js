976f10ad7ec2d9cc08d4e36281fef295bbf52fcf
Joe Webb <joewebb@Joes-MacBook-Pro.local>
2019-03-25 17:05:22 -0600

    edit profile complete

--- server/controllers/infocontroller.js
+++ server/controllers/infocontroller.js
@@ -1,92 +1,129 @@
-const OpenTok = require('opentok')
-const {OPENTOK_API_KEY,OPENTOK_API_SECRET} = process.env
+const OpenTok = require('opentok');
+const { OPENTOK_API_KEY, OPENTOK_API_SECRET } = process.env;
 const opentok = new OpenTok(OPENTOK_API_KEY, OPENTOK_API_SECRET);
 
-module.exports={
-    createNewCourse:async(req,res)=>{
-      const {title,subject_id,description,teacher_id,start_date,end_date,lectures} = req.body
-      const db = req.app.get('db')
-    opentok.createSession({mediaMode:'routed'},async function(err,session){
-        if(err)return console.log(err)
-        
-        const session_id=session.sessionId
+module.exports = {
+  createNewCourse: async (req, res) => {
+    const {
+      title,
+      subject_id,
+      description,
+      teacher_id,
+      start_date,
+      end_date,
+      lectures
+    } = req.body;
+    const db = req.app.get('db');
+    opentok.createSession({ mediaMode: 'routed' }, async function(
+      err,
+      session
+    ) {
+      if (err) return console.log(err);
 
-    
+      const session_id = session.sessionId;
 
-        const createNewCourse = await db.info.create.course([title,subject_id,description,teacher_id,start_date,end_date,session_id])
-        const course_id = createNewCourse[0].course_id;
+      const createNewCourse = await db.info.create.course([
+        title,
+        subject_id,
+        description,
+        teacher_id,
+        start_date,
+        end_date,
+        session_id
+      ]);
+      const course_id = createNewCourse[0].course_id;
 
-        for(let i=0;i<lectures.length;i++){
-            const lecture = lectures[i]
-            const {date,lecture_description,lecture_start_time,lecture_end_time} = lecture
+      for (let i = 0; i < lectures.length; i++) {
+        const lecture = lectures[i];
+        const {
+          date,
+          lecture_description,
+          lecture_start_time,
+          lecture_end_time
+        } = lecture;
 
-            const addLectures = await db.info.create.lecture([course_id,date,lecture_description,lecture_start_time,lecture_end_time]) 
-        }
+        const addLectures = await db.info.create.lecture([
+          course_id,
+          date,
+          lecture_description,
+          lecture_start_time,
+          lecture_end_time
+        ]);
+      }
 
-        res.sendStatus(201) 
-    })
+      res.sendStatus(201);
+    });
+  },
 
+  getAllCourses: async (req, res) => {
+    const db = req.app.get('db');
 
+    const courses = await db.info.getAllCourses();
 
+    res.send(courses);
+  },
 
-    },
+  getCoursesForUser: async (req, res) => {
+    const db = req.app.get('db');
+    const { user_id, is_teacher } = req.query;
 
-    getAllCourses:async(req,res)=>{
-        const db = req.app.get('db')
-
-        const courses = await db.info.getAllCourses()
-
-        res.send(courses)
-    },
-
-    getCoursesForUser:async(req,res)=>{
-        const db = req.app.get('db')
-        const {user_id,is_teacher} = req.query
-
-        if(is_teacher){
-            const courses = await db.info.getTeacherCourses([user_id])
-            res.send(courses)
-        }
-        else{
-            const courses =  await db.info.getStudentCourses([user_id])
-            res.send(courses)
-        }
-
-    },
-
-    getAllSubjects:async(req,res)=>{
-        const db = req.app.get('db')
-
-        const subjects = await db.info.getAllSubjects()
-        res.send(subjects)
-    },
-
-    generateToken:async(req,res)=>{
-        console.log('hit')
-        const db = req.app.get('db')
-        let {course_id} = req.params
-        course_id = parseInt(course_id)
-
-
-        let session_id = await db.info.getSessionId([course_id])
-
-        session_id = session_id[0].session_id
-
-        const token = opentok.generateToken(session_id)
-
-        res.send({token,session_id})
-    },
-
-    getLectureTimes: async (req, res)=>{
-        console.log('hit getLectureTimes')
-        const db = req.app.get('db')
-
-        let lectureTimes = await db.info.getLectureTimes()
-
-        console.log(lectureTimes)
-
-        res.send(lectureTimes).status(200)
-        
+    if (is_teacher) {
+      const courses = await db.info.getTeacherCourses([user_id]);
+      res.send(courses);
+    } else {
+      const courses = await db.info.getStudentCourses([user_id]);
+      res.send(courses);
     }
+  },
 
-}
+  getAllSubjects: async (req, res) => {
+    const db = req.app.get('db');
+
+    const subjects = await db.info.getAllSubjects();
+    res.send(subjects);
+  },
+
+  generateToken: async (req, res) => {
+    console.log('hit');
+    const db = req.app.get('db');
+    let { course_id } = req.params;
+    course_id = parseInt(course_id);
+
+    let session_id = await db.info.getSessionId([course_id]);
+
+    session_id = session_id[0].session_id;
+
+    const token = opentok.generateToken(session_id);
+
+    res.send({ token, session_id });
+  },
+
+  getLectureTimes: async (req, res) => {
+    console.log('hit getLectureTimes');
+    const db = req.app.get('db');
+
+    let lectureTimes = await db.info.getLectureTimes();
+
+    console.log(lectureTimes);
+
+    res.send(lectureTimes).status(200);
+  },
+  editProfileInfo: async (req, res) => {
+    try {
+      const db = req.app.get('db');
+      const { user_id, username, email, first_name, last_name } = req.body;
+      let editProfile = await db.info.editProfile([
+        user_id,
+        username,
+        email,
+        first_name,
+        last_name
+      ]);
+      console.log(req.body);
+      //   console.log(editProfile);
+      res.send(editProfile);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
--- src/Containers/ProfileInfoContainer/ProfileInfoContainer.js
+++ src/Containers/ProfileInfoContainer/ProfileInfoContainer.js
@@ -4,26 +4,26 @@ import './ProfileInfoContainer.scss';
 import CourseInfo from '../../Components/Dashboard/CourseInfo';
 import ProfileInfo from '../../Components/Dashboard/ProfileInfo';
 import SecondaryButton from './../../Components/Buttons/SecondaryButton';
-import {connect} from 'react-redux'
+import { connect } from 'react-redux';
 
 class ProfileInfoContainer extends Component {
   state = {
     toggleCourseInfo: false,
-    buttonName:this.props.is_teacher?'Teacher Info':'Student Info'
+    buttonName: this.props.is_teacher ? 'Teacher Info' : 'Student Info',
+    toggleEditProfile: false
   };
 
-  componentDidUpdate(prevProps){
-    if(prevProps !== this.props){
-      console.log('hit')
-      if(this.props.is_teacher){
+  componentDidUpdate(prevProps) {
+    if (prevProps !== this.props) {
+      console.log('hit');
+      if (this.props.is_teacher) {
         this.setState({
-          buttonName:'Teacher Info'
-        })
-      }
-      else{
+          buttonName: 'Teacher Info'
+        });
+      } else {
         this.setState({
-          buttonName:'Student Info'
-        })
+          buttonName: 'Student Info'
+        });
       }
     }
   }
@@ -38,25 +38,64 @@ class ProfileInfoContainer extends Component {
       toggleCourseInfo: false
     });
   };
+  setToggleEditProfileInfo = () => {
+    this.setState({
+      toggleEditProfile: !this.state.toggleEditProfile
+    });
+  };
   render() {
-    const { toggleCourseInfo,buttonName } = this.state;
+    const { toggleCourseInfo, buttonName, toggleEditProfile } = this.state;
     return (
       <div className='MainInfoContainer'>
         <div className='toggleButtons'>
-          <SecondaryButton onClick={this.setToggleStudentInfo} isActive={!toggleCourseInfo}>{buttonName}</SecondaryButton>
-          <SecondaryButton onClick={this.setToggleCourseInfo} isActive={toggleCourseInfo}>Course Info</SecondaryButton>
+          <SecondaryButton
+            onClick={this.setToggleStudentInfo}
+            isActive={!toggleCourseInfo}
+          >
+            {buttonName}
+          </SecondaryButton>
+          <SecondaryButton
+            onClick={this.setToggleCourseInfo}
+            isActive={toggleCourseInfo}
+          >
+            Course Info
+          </SecondaryButton>
         </div>
-        {!toggleCourseInfo ? <ProfileInfo /> : <CourseInfo />}
+        {!toggleCourseInfo ? (
+          <ProfileInfo editActive={this.state.toggleEditProfile} />
+        ) : (
+          <CourseInfo />
+        )}
+        {/* //Edit Profile Button */}
+        <SecondaryButton
+          onClick={this.setToggleEditProfileInfo}
+          isActive={toggleEditProfile}
+        >
+          Edit:
+          <img
+            style={{
+              height: '20px',
+              border: '1px solid black',
+              borderRadius: '20%',
+              padding: '5%'
+            }}
+            alt='edit profile'
+            src={`https://icongr.am/entypo/edit.svg`}
+          />
+        </SecondaryButton>
       </div>
     );
   }
 }
 
-const m2p = (state) => {
-  const {is_teacher} = state
-  return{
+const m2p = state => {
+  const { is_teacher } = state;
+  return {
     is_teacher
-  }
-}
+  };
+};
 
-export default connect(m2p,null)(ProfileInfoContainer)
+export default connect(
+  m2p,
+  null
+)(ProfileInfoContainer);
