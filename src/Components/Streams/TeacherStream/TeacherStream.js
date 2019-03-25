12c277c3ae7dc1744df7d7e5cbf8d4ee7c04dd23
Your Name <you@example.com>
2019-03-25 09:07:35 -0600

    changed routing, added student and teacher stream

---
+++ src/Components/Streams/TeacherStream/TeacherStream.js
@@ -0,0 +1,13 @@
+import React,{useEffect,useState} from 'react';
+
+
+const TeacherStream = (props) => {
+  return(
+    <div>
+      Teacher Stream
+    </div>
+  )
+}
+
+export default TeacherStream
+
--- src/Pages/Classroom/Classroom.js
+++ src/Pages/Classroom/Classroom.js
@@ -7,22 +7,23 @@ import Axios from 'axios';
 const Classroom =(props)=>{
     const {course_id} = props.match.params
     const [token,setToken] = useState('')
+    const [session_id,setSessionId] = useState('') 
 
     useEffect(()=>{
-        if(token===''){
-            Axios.post(`/info/generatetoken/${course_id}`).then((token)=>{
-                token = token.data
-                setToken(token)
+        if(token === '' || session_id === ''){
+            Axios.post(`/info/generatetoken/${course_id}`).then((res)=>{
+                setToken(res.data.token)
+                setSessionId(res.data.session_id)
             })
         }
-    })
+    },[])
 
 
     return(
         <div className='classroom_main'>
             <div className='video_qchat_container'>
                 <div className='video_container'>
-                    <VideoContainer token={token}/>
+                    {token !=='' && session_id && <VideoContainer token={token} session_id={session_id}/>}                    
                 </div>
                 <div className='qchat_container'>
                     <QueueChatContainer/>
