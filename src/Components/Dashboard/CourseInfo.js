6fecae5d4cdec32e19ae94c197637279adba13e5
Joe Webb <joewebb@Joes-MacBook-Pro.local>
2019-04-03 10:35:38 -0600

    edit course info working front to back

--- src/Components/Dashboard/CourseInfo.js
+++ src/Components/Dashboard/CourseInfo.js
@@ -1,41 +1,123 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './InfoContainerStyling.scss';
 import { NavLink } from 'react-router-dom';
 import PrimaryButton from '../../Components/Buttons/PrimaryButton';
 import { connect } from 'react-redux';
+import ReactQuill from 'react-quill';
+import { updateCourseInfo } from '../../ducks/reducer';
+import axios from 'axios';
 import AddLectureModal from '../../Containers/AddCourseModal/AddLectureModal';
 
-function CourseInfo(props) {
-  console.log('courseInfo', props);
-  const { course } = props;
+class CourseInfo extends Component {
+  // console.log('courseInfo', props);
   // const {title,description,subject_id,teacher_id,start_date,end_date} = course
-  return (
-    <>
-      {course ? (
-        <div className='infoContainer'>
-          <h4>Course Title:</h4>
-          <p>{course.title}</p>
-          <h4>Course Description:</h4>
-          <p dangerouslySetInnerHTML={{ __html: course.description }} />
-          {props.is_teacher ? (
-            <NavLink to='/addLecture'>
-              <PrimaryButton>Add Lecture</PrimaryButton>
+  state = {
+    title: '',
+    description: '',
+    editing: false
+  };
+  edit = () => {
+    this.setState({
+      editing: !this.state.editing
+    });
+  };
+  handlelectureDescInput = val => {
+    this.setState({
+      description: val
+    });
+  };
+  handleTitleInput = val => {
+    console.log(val);
+
+    this.setState({
+      title: val
+    });
+  };
+  submitEdit = () => {
+    const { title, description } = this.state;
+    // const { course_id } = this.props;
+    // console.log('props', this.props);
+    // console.log(666, this.state);
+    axios
+      .put(`/api/updateCourse/${this.props.course.course_id}`, {
+        title,
+        description
+      })
+      .then(res => {
+        this.props.updateCourseInfo(res.data[0]);
+        this.setState({
+          editing: !this.state.editing
+        });
+        console.log(res);
+      });
+  };
+  render() {
+    const { course } = this.props;
+    return (
+      <div>
+        {!this.state.editing ? (
+          <>
+            {course ? (
+              <div className='infoContainer'>
+                <h4>Course Title:</h4>
+                <p>{course.title}</p>
+                <h4>Course Description:</h4>
+                <p dangerouslySetInnerHTML={{ __html: course.description }} />
+                {this.props.is_teacher ? (
+                  <NavLink to='/addLecture'>
+                    <PrimaryButton>Add Lecture</PrimaryButton>
+                  </NavLink>
+                ) : (
+                  <></>
+                )}
+                {/* <AddLectureModal /> */}
+                <br />
+                <NavLink to={`/classroom/${course.course_id}`}>
+                  <PrimaryButton>Enter Classroom</PrimaryButton>
+                </NavLink>
+              </div>
+            ) : (
+              <div className='infoContainer'>No course selected</div>
+            )}
+          </>
+        ) : (
+          <div className='infoContainer'>
+            <button onClick={this.submitEdit}>Submit</button>
+            <h4>Course Title:</h4>
+            <input
+              value={this.state.title}
+              onChange={e => this.handleTitleInput(e.target.value)}
+            />
+            <h4>Course Description:</h4>
+            <ReactQuill
+              className='react-quill'
+              value={this.state.description}
+              name='lecDescription'
+              onChange={this.handlelectureDescInput}
+              style={{ height: '40vh', width: '90vw' }}
+            />
+            {/* {this.props.is_teacher ? (
+              <NavLink to='/addLecture'>
+                <PrimaryButton>Add Lecture</PrimaryButton>
+              </NavLink>
+            ) : (
+              <></>
+            )} */}
+            {/* <AddLectureModal /> */}
+            <br />
+            <NavLink to={`/classroom/${course.course_id}`}>
+              <PrimaryButton>Enter Classroom</PrimaryButton>
             </NavLink>
-          ) : (
-            <></>
-          )}
-          {/* <AddLectureModal /> */}
-          <br />
-          <NavLink to={`/classroom/${course.course_id}`}>
-            <PrimaryButton>Enter Classroom</PrimaryButton>
-          </NavLink>
+          </div>
+        )}
+        <div>
+          <button onClick={this.edit} style={{ backgroundColor: 'aqua' }}>
+            Edit Stuff:
+          </button>
         </div>
-      ) : (
-        <div className='infoContainer'>No course selected</div>
-      )}
-      <button style={{ backgroundColor: 'aqua' }}>Edit Stuff:</button>
-    </>
-  );
+      </div>
+    );
+  }
 }
 
 const m2p = state => {
@@ -48,5 +130,5 @@ const m2p = state => {
 
 export default connect(
   m2p,
-  null
+  { updateCourseInfo }
 )(CourseInfo);
