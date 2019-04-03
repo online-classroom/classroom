98e32b9515add4380ae26f4da640c8e4e1aa32a4
gprakarsh <379@doonschool.com>
2019-04-03 09:39:20 -0600

    screen sharing configured

--- src/Containers/VideoContainer/VideoContainer.js
+++ src/Containers/VideoContainer/VideoContainer.js
@@ -1,8 +1,9 @@
-import React,{useEffect, memo} from "react";
+import React,{useEffect,useState, memo} from "react";
 import { connect } from "react-redux";
 import TeacherStream from "./../../Components/Streams/TeacherStream/TeacherStream";
 import StudentStream from "./../../Components/Streams/StudentStream/StudentStream";
-import {updateUser} from './../../ducks/reducer'
+import {updateUser} from './../../ducks/reducer';
+import LectureVideos from './../../Components/LectureVideos/LectureVideos';
 
 const VideoContainer = (props) => {
   const { is_teacher, token, session_id, socket, queue, course_id} = props;
@@ -33,7 +34,8 @@ const VideoContainer = (props) => {
     { webRTC 
     ?
       is_teacher ? <TeacherStream  course_id={course_id} token={token} session_id={session_id} queue={queue} socket={socket} /> : <StudentStream token={token} session_id={session_id} queue={queue} socket={socket} course_id={course_id}/>
-    :
+    
+      :
       <LectureVideos course_id={course_id} />
     }
     </div>
