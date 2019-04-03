3d114fdf2e6590c7515551dbc44863e6a62286a1
gprakarsh <379@doonschool.com>
2019-04-03 14:14:05 -0600

    fixed access denied error

--- src/Pages/Classroom/Classroom.js
+++ src/Pages/Classroom/Classroom.js
@@ -53,7 +53,6 @@ const Classroom = props => {
     try{
       const lRes = await Axios.get(`/info/course/privacy/?user_id=${user_id}&is_teacher=${is_teacher}&course_id=${course_id}`)
     }catch{
-        alert('Access Denied!')
         props.history.push('/dashboard')
     }
   }
