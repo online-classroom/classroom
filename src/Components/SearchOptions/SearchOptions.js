633260c48dad627ac20af24631135d188fa213a5
gprakarsh <379@doonschool.com>
2019-04-03 12:07:22 -0600

    added search feature

---
+++ src/Components/SearchOptions/SearchOptions.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import './SearchOptions.scss'
+import Axios from 'axios';
+
+const SearchOptions = (props) => {
+    const [subjects,setSubjects] = useState([])
+    const [courses, setCourses] = useState([])
+    const {searchString} = props
+
+    useEffect(()=>{
+        getCoursesAndSubjects()
+    },[])
+
+    useEffect(()=>{
+        filter()
+    },[searchString])
+
+    const filter = ()=> {
+        // Declare variables
+        
+        // input = document.getElementById('myInput');
+        const filter = searchString.toUpperCase();
+        
+        const li = document.getElementsByClassName('course-title');
+
+        for(let i=0;i<li.length;i++){
+          const txtValue = li[i].textContent || li[i].innerText
+          if(txtValue.toUpperCase().indexOf(filter) > -1){
+            li[i].style.display = ''    
+          }
+          else{
+              li[i].style.display = 'none'
+          }
+        }
+      
+        // Loop through all list items, and hide those who don't match the search query
+        
+      }
+    
+
+    const getCoursesAndSubjects = async() =>{
+        const cRes = await Axios.get(`/info/courses`)
+        setCourses(cRes.data)
+        const sRes = await Axios.get(`/info/subjects`)
+        setSubjects(sRes.data) 
+    }
+    
+    const mappedCourses = (id) =>{
+        const mapper = courses.filter((course)=>course.subject_id===id).map((course)=>{
+            return(
+                <p id='course-title' className='course-title'>{course.title}</p>
+            )
+        })
+        return mapper
+    }
+
+    const mappedSubjects = subjects.map((subject)=>{
+        return(
+            <div className='subject-container' id='subject-container'>
+                <p id='subject-name'>{subject.subject_name}</p>
+                <div className='courses' id='courses' >{mappedCourses(subject.subject_id)}</div>
+            </div>
+        )
+    })
+
+
+    return(
+        <div className='searchOptions'>
+            {mappedSubjects}
+        </div>
+    )
+}
+
+export default SearchOptions;
