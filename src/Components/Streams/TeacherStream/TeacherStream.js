d049a719416db8d6520711bc2cbae9381b69ab7c
elliot <elliotgcraig@icloud.com>
2019-04-04 09:29:33 -0600

    about to pull

--- src/Components/Streams/TeacherStream/TeacherStream.js
+++ src/Components/Streams/TeacherStream/TeacherStream.js
@@ -9,12 +9,12 @@ import SecondaryButton from "../../Buttons/SecondaryButton";
 
 const TeacherStream = props => {
   const [streams, setStreams] = useState([]);
-  const [publish,setPublish] = useState(false)
-  const [lectures,setLectures] = useState([])
-  const [selectedLecture,setSelectedLecture] = useState({})
-  const [record,setRecord] = useState(false)
-  const {session_id, token, course_id} = props;
-  const [source,setSource] = useState('')
+  const [publish, setPublish] = useState(false)
+  const [lectures, setLectures] = useState([])
+  const [selectedLecture, setSelectedLecture] = useState({})
+  const [record, setRecord] = useState(false)
+  const { session_id, token, course_id } = props;
+  const [source, setSource] = useState('')
 
   const sessionHelper = createSession({
     apiKey: process.env.REACT_APP_OPENTOK_API_KEY,
@@ -27,14 +27,14 @@ const TeacherStream = props => {
   // let sessionHelper = {}
 
   useEffect(() => {
-    
+
     getLectures()
     return () => {
       sessionHelper.disconnect();
     };
   }, []);
 
-  const getLectures = async()=>{
+  const getLectures = async () => {
     const lRes = await Axios.get(`/info/lectures/course/${course_id}`)
     setLectures(lRes.data)
   }
@@ -49,40 +49,40 @@ const TeacherStream = props => {
     );
   })
 
-  const mappedLectures = lectures.filter((lecture)=>lecture.archive_id===null).map((lecture,i)=>{
-    return(
-      <SecondaryButton key={i} onClick={()=>setSelectedLecture(lecture)} isActive={selectedLecture===lecture}>
-        <div dangerouslySetInnerHTML={{ __html: lecture.lecture_description }}/>    
+  const mappedLectures = lectures.filter((lecture) => lecture.archive_id === null).map((lecture, i) => {
+    return (
+      <SecondaryButton key={i} onClick={() => setSelectedLecture(lecture)} isActive={selectedLecture === lecture}>
+        <div dangerouslySetInnerHTML={{ __html: lecture.lecture_description }} />
       </SecondaryButton>
     )
   })
 
-  const mappedRecordedLectures = lectures.filter((lecture)=>lecture.archive_id!==null).map((lecture,i)=>{
-    return(
+  const mappedRecordedLectures = lectures.filter((lecture) => lecture.archive_id !== null).map((lecture, i) => {
+    return (
       <p key={i}>
-        <div dangerouslySetInnerHTML={{ __html: lecture.lecture_description }}/>    
+        <div dangerouslySetInnerHTML={{ __html: lecture.lecture_description }} />
       </p>
     )
   })
 
-  const startStream = async() => {
-    
-    if(Object.keys(selectedLecture).length===0){
+  const startStream = async () => {
+
+    if (Object.keys(selectedLecture).length === 0) {
       return alert("You must select a lecture to start streaming")
     }
-    else{
-      if(record){
-        const startRecording = await Axios.post(`/archive/record/start`,{session_id, lecture_id:selectedLecture.lecture_id, description:selectedLecture.lecture_description})
+    else {
+      if (record) {
+        const startRecording = await Axios.post(`/archive/record/start`, { session_id, lecture_id: selectedLecture.lecture_id, description: selectedLecture.lecture_description })
       }
       setPublish(true)
     }
 
   }
 
-  const stopStream = async() => {
-    
+  const stopStream = async () => {
+
     setPublish(false)
-    const stopRecording = await Axios.post(`/archive/record/stop`,{lecture_id:selectedLecture.lecture_id})
+    const stopRecording = await Axios.post(`/archive/record/stop`, { lecture_id: selectedLecture.lecture_id })
     window.location.reload()
 
   }
@@ -90,34 +90,34 @@ const TeacherStream = props => {
 
   return (
     <div className='teacherStream'>
-    {publish
-      ? <div>
-        {source==='screen'
-      ?
-      <OTPublisher properties={{width: '100%', height: '58vh', name:'Teacher', videoSource:'screen'}} session={sessionHelper.session} />
-      :
-      <OTPublisher properties={{width: '100%', height: '58vh', name:'Teacher'}} session={sessionHelper.session} />
-    } 
-      {mappedStreams}
-      <PrimaryButton onClick={stopStream}>End Lecture</PrimaryButton>
-      <SecondaryButton isActive={source==='screen'} onClick={()=>setSource('screen')}>Screen</SecondaryButton>
-      <SecondaryButton isActive={source===''} onClick={()=>setSource('')}>Camera</SecondaryButton>
-      </div>
-      : <div>
-        <div>
-          <p>Choose Display:</p>
-          <SecondaryButton isActive={source==='screen'} onClick={()=>setSource('screen')}>Screen</SecondaryButton>
-          <SecondaryButton isActive={source===''} onClick={()=>setSource('')}>Camera</SecondaryButton>
+      {publish
+        ? <div>
+          {source === 'screen'
+            ?
+            <OTPublisher properties={{ width: '100%', height: '58vh', name: 'Teacher', videoSource: 'screen' }} session={sessionHelper.session} />
+            :
+            <OTPublisher properties={{ width: '100%', height: '58vh', name: 'Teacher' }} session={sessionHelper.session} />
+          }
+          {mappedStreams}
+          <PrimaryButton onClick={stopStream}>End Lecture</PrimaryButton>
+          <SecondaryButton isActive={source === 'screen'} onClick={() => setSource('screen')}>Screen</SecondaryButton>
+          <SecondaryButton isActive={source === ''} onClick={() => setSource('')}>Camera</SecondaryButton>
         </div>
-        <p>Scheduled Lectures</p>
-        <div>{mappedLectures}</div>
-        <p>Recorded Lectures</p>
-        <div>{mappedRecordedLectures}</div>
-        <p>Check to record this lecture</p>
-        <input type='checkbox' onChange={()=>setRecord(!record)} value={record}/>
-        <PrimaryButton onClick={startStream}>Start Lecture</PrimaryButton>
-      </div>
-    }
+        : <div>
+          <div>
+            <p>Choose Display:</p>
+            <SecondaryButton isActive={source === 'screen'} onClick={() => setSource('screen')}>Screen</SecondaryButton>
+            <SecondaryButton isActive={source === ''} onClick={() => setSource('')}>Camera</SecondaryButton>
+          </div>
+          <p>Scheduled Lectures</p>
+          <div>{mappedLectures}</div>
+          <p>Recorded Lectures</p>
+          <div>{mappedRecordedLectures}</div>
+          <p>Check to record this lecture</p>
+          <input type='checkbox' onChange={() => setRecord(!record)} value={record} />
+          <PrimaryButton onClick={startStream}>Start Lecture</PrimaryButton>
+        </div>
+      }
     </div>
   );
 };
